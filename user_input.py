83e9b35460b4cfb82d08c5aa12043b265b764cef
thanders <thanders@alumni.purdue.edu>
2018-04-28 17:19:40 +0100

    Changed storage of user input from a set to dictionary so that I can store the price of a trip (fuel weighted distance)

--- user_input.py
+++ user_input.py
@@ -1,9 +1,11 @@
 
+
 class UserInput:
     def __init__(self, airports_dict):
         # Variables to store OWM connection data:
         self.airports_dict = airports_dict
-        self.itinerary_set = set([])
+        #self.itinerary_set = set([])
+        self.itinerary_dict = {}
         self.size_loop = True
         self.itinerary_loop = True
         self.input_size = None
@@ -32,11 +34,12 @@ class UserInput:
     @staticmethod
     def desc_program():
 
-        print("Please enter up to four destinations (airport ICAO codes).")
+        print("Please enter up to four destinations.")
         print()
         print("Note:")
-        print("- This calculates a round trip so the first code you enter is both your starting point and destination")
-        print("- ICAO codes consist of four capital letters")
+        print("- Destinations must be in the format of IATA airport codes")
+        print("- IATA codes consist of three alphabetic capital letters")
+        print("- This program calculates a round trip. The first code you enter is both your starting point and destination")
         print()
         return 0
 
@@ -47,19 +50,20 @@ class UserInput:
 
             if len(input_icao) == 3 and input_icao.isalpha():
 
-                if (input_icao not in self.itinerary_set) and (len(self.itinerary_set) < (self.input_size-1)):
+                if (input_icao not in self.itinerary_dict) and (len(self.itinerary_dict) < (self.input_size-1)):
                     if input_icao in self.airports_dict:
-                        self.itinerary_set.add(input_icao)
-                        print("Set", self.itinerary_set, "Size:", len(self.itinerary_set))
+                        self.itinerary_dict[input_icao] = None
+                        print("Dictionary", self.itinerary_dict, "Size:", len(self.itinerary_dict))
                     else:
                         print("Airport code not in dictionary")
                         # raise ValueError("Airport code not in dictionary")
 
-                elif len(self.itinerary_set) == (self.input_size-1) and (input_icao not in self.itinerary_set):
+                elif len(self.itinerary_dict) == (self.input_size-1) and (input_icao not in self.itinerary_dict):
 
                     if input_icao in self.airports_dict:
-                        self.itinerary_set.add(input_icao)
-                        print("Set", self.itinerary_set, "Size:", len(self.itinerary_set))
+                        self.itinerary_dict[input_icao] = None
+
+                        print("Dictionary", self.itinerary_dict, "Size:", len(self.itinerary_dict))
                         print()
                         print("Thank-you, I will now calculate the shortest path (fuel price weighted)")
                         self.itinerary_loop = False
@@ -68,7 +72,7 @@ class UserInput:
                         print("Airport code does not exist")
 
                 else:
-                    print("Sorry that one is already in the set")
+                    print("Sorry that one is already in the dictionary")
                     # raise ValueError("Sorry, I don't understand your input")
             else:
                 print('Inputted ICAO code is invalid. It must be four alphabetic characters')
